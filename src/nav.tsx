1c9c62e76d79d66b2646b12c1170610343083dd1
Emmanuelomolaju <emmanuelomolaju964@gmail.com>
2025-03-14 21:14:45 +0100

    fix(nav): adjust class for improved responsive behavior

--- src/nav.tsx
+++ src/nav.tsx
@@ -62,7 +62,7 @@ const Nav = () => {
 
       <div
         data-open={isOpen ? "true" : "false"}
-        className="data-[open=false]:h-0 data-[open=false]:p-0 overflow-hidden transition-all absolute inset-x-0 z-20 w-full bg-white px-6 py-4 shadow-md transition-all duration-300 ease-in-out dark:bg-gray-900 lg:relative lg:top-0 lg:mt-0 lg:flex lg:w-auto lg:translate-x-0 lg:items-center lg:bg-transparent lg:p-0 lg:opacity-100 lg:shadow-none lg:dark:bg-transparent"
+        className="data-[open=false]:h-0 data-[open=false]:p-0 overflow-hidden lg:data-[open=false]:h-auto lg:data-[open=false]:p-auto absolute inset-x-0 z-20 w-full bg-white px-6 py-4 shadow-md transition-all duration-300 ease-in-out dark:bg-gray-900 lg:relative lg:top-0 lg:mt-0 lg:flex lg:w-auto lg:translate-x-0 lg:items-center lg:bg-transparent lg:p-0 lg:opacity-100 lg:shadow-none lg:dark:bg-transparent"
       >
         <div className="lg:-px-8 flex flex-col space-y-4 lg:mt-0 lg:flex-row lg:space-y-0">
           <a
